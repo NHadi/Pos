b5c83c7db006ac729c248419cab3793aa55cae69
Nurul Hadi <n.hadi1996@gmail.com>
2019-11-03 05:41:28 +0700

    Add Mapping Profile with AutoMapper

---
+++ src/Pos.Customer.WebApi/Mapping/CommandToDomainMapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Pos.Customer.Domain.CustomerAggregate;
+using Pos.Customer.WebApi.Application.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pos.Customer.WebApi.Mapping
+{
+    public class CommandToDomainMapperProfile : Profile
+    {
+        public CommandToDomainMapperProfile()
+        {
+            CreateMap<CreateCustomerCommand, MstCustomer>()
+                .ForMember(x => x.Id, o => o.MapFrom(s => Guid.NewGuid()));                
+        }
+    }
+}
