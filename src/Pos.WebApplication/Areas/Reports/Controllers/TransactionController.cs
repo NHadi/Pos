bb8378c1d5cddada9205ae0b6b6a7061ec997261
Nurul Hadi <n.hadi1996@gmail.com>
2019-11-04 10:26:45 +0700

    add prototype

---
+++ src/Pos.WebApplication/Areas/Reports/Controllers/TransactionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pos.WebApplication.Areas.Masters.Controllers
+{
+    [Area("Reports")]
+    public class TransactionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
