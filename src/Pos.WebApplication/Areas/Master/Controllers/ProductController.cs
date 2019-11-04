49e2596ce7a414d0243bd66fd9f4df36f1e06254
Nurul Hadi <n.hadi1996@gmail.com>
2019-11-04 10:24:55 +0700

    add prototype UI Application

---
+++ src/Pos.WebApplication/Areas/Master/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pos.WebApplication.Areas.Masters.Controllers
+{
+    [Area("Master")]
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
